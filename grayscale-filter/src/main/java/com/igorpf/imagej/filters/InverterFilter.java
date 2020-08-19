db6e192b2fada84c6b2abc8974dd029de8459e1e
Igor Pires Ferreira <ipferreira@inf.ufrgs.br>
2020-08-19 08:35:52 -0300

    Compilable Java setup

---
+++ grayscale-filter/src/main/java/com/igorpf/imagej/filters/InverterFilter.java
@@ -0,0 +1,13 @@
+package com.igorpf.imagej.filters;
+
+import ij.*;
+
+public class InverterFilter {
+    public InverterFilter() {
+        ImageJ ij = new ImageJ();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
